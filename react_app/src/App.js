a5f1e2e63572cee7c62bd938c2f6daa6e064894d
KazumichiShirai <acdcacdc1985@hotmail.com>
2020-01-18 23:06:35 +0900

    Use Redux. Section 4-1

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,74 +1,88 @@
 import React, { Component } from 'react';
-import Rect from './Rect';
+import { connect } from 'react-redux';
 import './App.css';
 
 
-class App extends Component {
-  input = '';
-
-
-  msgStyle = {
-    fontSize:"20pt",
-    color:"#900",
-    margin:"20px 0px",
-    padding: "5px",
-  }
-
-
-  constructor(props){
-    super(props);
-    this.state = {
-      message:'type your name:'
-    };
-    this.doCheck = this.doCheck.bind(this);
-  }
-
-
-  doCheck(event) {
-    alert(event.target.value +
-      "は長すぎます。(最大10文字)");
-  }
-
-
-  render(){
-    return <div>
-      <h1>React</h1>
-      <h2>{this.state.message}</h2>
-      <Message maxlength="10" onCheck={this.doCheck} />
-    </div>;
-  }
-
-
+// ステートのマッピング
+function mappingState(state) {
+  return state;
 }
 
 
-class Message extends Component {
-  inputStyle = {
-    fontSize:"12pt",
-    padding:"5px"
-  }
+// Appコンポーネント
+class App extends Component {
 
 
   constructor(props){
     super(props);
-    this.doChange = this.doChange.bind(this);
   }
 
+  render() {
+    return (
+      <div>
+        <h1>Redux</h1>
+        <Message />
+        <Button />
+      </div>
+    );
+  }
+}
+// ストアのコネクト
+App = connect()(App);
 
-  doChange(e){
-    if (e.target.value.length > this.props.maxlength){
-      this.props.onCheck(e);
-      e.target.value =
-        e.target.value.substr(0,this.props.maxlength);
+
+// メッセージ表示のコンポーネント
+class Message extends Component {
+  style = {
+    fontSize:"20pt",
+    padding:"20px 5px"
+  }
+
+  render(){
+    return (
+      <p style={this.style}>
+        {this.props.message}: {this.props.counter}
+      </p>
+    );
+  }
+}
+// ストアのコネクト
+Message = connect(mappingState)(Message);
+
+
+//
+class Button extends Component {
+  style = {
+    fontSize:"16pt",
+    padding:"5px 10px"
+  }
+
+  constructor(props){
+    super(props);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  // ボタンクリックでディスパッチを実行
+  doAction(e){
+    if (e.shiftKey){
+      this.props.dispatch({ type:'DECREMENT' });
+    } else {
+      this.props.dispatch({ type:'INCREMENT' });
     }
   }
 
 
   render(){
-    return <input type="text" style={this.inputStyle}
-      onChange={this.doChange} />
+    return (
+      <button style={this.style}
+          onClick={this.doAction}>
+        click
+      </button>
+    );
   }
 }
+// ストアのコネクト
+Button = connect()(Button);
 
 
 export default App;
